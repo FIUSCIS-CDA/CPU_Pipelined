6e1f4c7370e40f1a5e95217e4c5f7e6d3aa7da28
movingpictures83 <movingpictures83@gmail.com>
2022-05-18 17:13:16 -0400

    New testbench design

--- ForwardDetection/ForwardBfromMEMtoEX.v
+++ ForwardDetection/ForwardBfromMEMtoEX.v
@@ -1,21 +1,21 @@
-// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
-// and other software and tools, and its AMPP partner logic 
+// Copyright (C) 2020  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and any partner logic 
 // functions, and any output files from any of the foregoing 
 // (including device programming or simulation files), and any 
 // associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus II License Agreement,
-// the Altera MegaCore Function License Agreement, or other 
-// applicable license agreement, including, without limitation, 
-// that your use is for the sole purpose of programming logic 
-// devices manufactured by Altera and sold by Altera or its 
-// authorized distributors.  Please refer to the applicable 
-// agreement for further details.
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details, at
+// https://fpgasoftware.intel.com/eula.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
-// CREATED		"Tue Feb  2 08:05:38 2021"
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
+// CREATED		"Wed May 18 06:49:42 2022"
 
 module ForwardBfromMEMtoEX(
 	EXrd,
@@ -44,15 +44,24 @@ wire	MEMopisRTYPE;
 wire	MEMopisRTYPE_and_MEMrdisEXrt;
 wire	MEMrdisEXrt;
 wire	MEMrtisEXrt;
+wire	SYNTHESIZED_WIRE_0;
+wire	SYNTHESIZED_WIRE_1;
 
 
 
+
+
+SLTI	b2v_inst(
+	.Op(MEMop),
+	.slti_output(SYNTHESIZED_WIRE_1));
 
 assign	Y = MEMopisADDI_and_MEMrtisEXrt | MEMopisRTYPE_and_MEMrdisEXrt;
 
 assign	MEMopisRTYPE_and_MEMrdisEXrt = MEMopisRTYPE & MEMrdisEXrt;
 
-assign	MEMopisADDI_and_MEMrtisEXrt = MEMopisADDI & MEMrtisEXrt;
+assign	MEMopisADDI_and_MEMrtisEXrt = SYNTHESIZED_WIRE_0 & MEMrtisEXrt;
+
+assign	SYNTHESIZED_WIRE_0 = SYNTHESIZED_WIRE_1 | MEMopisADDI;
 
 
 ADDI	b2v_MEMopADDI(
