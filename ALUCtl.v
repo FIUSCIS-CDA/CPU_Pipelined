b5a6377a95b6c209213b16d61d79249eb982bc0a
movingpictures83 <movingpictures83@gmail.com>
2022-05-18 17:13:15 -0400

    New testbench design

--- ALUCtl.v
+++ ALUCtl.v
@@ -1,21 +1,21 @@
-// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
-// and other software and tools, and its AMPP partner logic 
+// Copyright (C) 2020  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and any partner logic 
 // functions, and any output files from any of the foregoing 
 // (including device programming or simulation files), and any 
 // associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus II License Agreement,
-// the Altera MegaCore Function License Agreement, or other 
-// applicable license agreement, including, without limitation, 
-// that your use is for the sole purpose of programming logic 
-// devices manufactured by Altera and sold by Altera or its 
-// authorized distributors.  Please refer to the applicable 
-// agreement for further details.
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details, at
+// https://fpgasoftware.intel.com/eula.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
-// CREATED		"Tue Feb  2 08:00:57 2021"
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
+// CREATED		"Wed May 18 05:20:45 2022"
 
 module ALUCtl(
 	Functcode,
@@ -36,16 +36,25 @@ wire	ORinstruction;
 wire	SLTinstruction;
 wire	SUBinstruction;
 wire	SWinstruction;
+wire	SYNTHESIZED_WIRE_4;
+wire	SYNTHESIZED_WIRE_3;
 
 
 
 
 
-assign	ALUOp_ALTERA_SYNTHESIZED[1] = LWinstruction | ADDinstruction | SWinstruction | SUBinstruction | SLTinstruction | ADDIinstruction;
 
-assign	ALUOp_ALTERA_SYNTHESIZED[0] = ORinstruction | SLTinstruction;
+SLTI	b2v_inst1(
+	.Op(Opcode),
+	.slti_output(SYNTHESIZED_WIRE_4));
 
-assign	ALUOp_ALTERA_SYNTHESIZED[3] = SLTinstruction | SUBinstruction;
+assign	SYNTHESIZED_WIRE_3 = LWinstruction | ADDinstruction | SWinstruction | SUBinstruction | SLTinstruction | ADDIinstruction;
+
+assign	ALUOp_ALTERA_SYNTHESIZED[3] = SLTinstruction | SYNTHESIZED_WIRE_4 | SUBinstruction;
+
+assign	ALUOp_ALTERA_SYNTHESIZED[0] = SYNTHESIZED_WIRE_4 | ORinstruction | SLTinstruction;
+
+assign	ALUOp_ALTERA_SYNTHESIZED[1] = SYNTHESIZED_WIRE_4 | SYNTHESIZED_WIRE_3;
 
 
 ADD	b2v_isADD(
