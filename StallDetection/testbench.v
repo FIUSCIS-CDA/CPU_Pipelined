73817c2b022b00ce435ea474ae4fb2481ce6560b
Trevor Cickovski <movingpictures83@gmail.com>
2024-01-04 13:29:07 -0500

    Added LUI

--- StallDetection/testbench.v
+++ StallDetection/testbench.v
@@ -11,9 +11,9 @@ module testbench();
 `include "../../Test/Test.v"
 ///////////////////////////////////////////////////////////////////////////////////
 // Inputs: IDop, EXop, MEMop (6-bit)
-//         EXrd, EXrs, EXrt, IDs, IDrt, MEMrt (5-bit)
+//         EXrp, EXrm, EXrn, IDs, IDrn, MEMrn (5-bit)
   reg[5:0] IDop, EXop, MEMop;
-  reg[4:0] EXrd, EXrs, EXrt, IDrs, IDrt, MEMrt;
+  reg[4:0] EXrp, EXrm, EXrn, IDrm, IDrn, MEMrn;
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 // Outputs: IDstall, EXstall (1-bit)
@@ -30,6 +30,7 @@ module testbench();
    parameter J = 6'b000010;
    parameter ADDI = 6'b001000;
    parameter SLTI = 6'b001010;
+   parameter LUI = 6'b001111;
    ///////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////
@@ -41,12 +42,12 @@ task initialState;
       IDop = 6'b000000;
       EXop = 6'b000000;
       MEMop = 6'b000000;      
-      EXrd = 5'b00001;
-      EXrs = 5'b00010;
-      EXrt = 5'b00011;
-      IDrs = 5'b00100;
-      IDrt = 5'b00101;
-      MEMrt = 5'b00110;
+      EXrp = 5'b00001;
+      EXrm = 5'b00010;
+      EXrn = 5'b00011;
+      IDrm = 5'b00100;
+      IDrn = 5'b00101;
+      MEMrn = 5'b00110;
    end
 endtask
 ///////////////////////////////////////////////////////////////////////////
@@ -72,119 +73,135 @@ end
 endtask
 ///////////////////////////////////////////////////////////////////////////
 	
-StallDetection stallDetector(.EXop(EXop), .EXrd(EXrd), .EXrs(EXrs), .EXrt(EXrt), 
-                             .IDop(IDop), .IDrs(IDrs), .IDrt(IDrt), 
-                             .MEMop(MEMop), .MEMrt(MEMrt), .IDStall(IDstall), .EXStall(EXstall));
+StallDetection stallDetector(.EXop(EXop), .EXrp(EXrp), .EXrm(EXrm), .EXrn(EXrn), 
+                             .IDop(IDop), .IDrm(IDrm), .IDrn(IDrn), 
+                             .MEMop(MEMop), .MEMrn(MEMrn), .IDStall(IDstall), .EXStall(EXstall));
    
    initial begin
 
    ///////////////////////////////////////////////////////////////////////////////
    // IDSTALL TESTS (Time: 20 each)
-   $display("[IDSTALL] IDop=BEQ, EXop=RTYPE, EXrd=IDrs");
-   setupStall(IDstall, IDop, BEQ, EXop, RTYPE, EXrd, IDrs); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=RTYPE, EXrp=IDrm");
+   setupStall(IDstall, IDop, BEQ, EXop, RTYPE, EXrp, IDrm); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, EXop=RTYPE, EXrd=IDrt");
-   setupStall(IDstall, IDop, BEQ, EXop, RTYPE, EXrd, IDrt); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=RTYPE, EXrp=IDrn");
+   setupStall(IDstall, IDop, BEQ, EXop, RTYPE, EXrp, IDrn); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, EXop=LW, EXrt=IDrs");
-   setupStall(IDstall, IDop, BEQ, EXop, LW, EXrt, IDrs); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=LW, EXrn=IDrm");
+   setupStall(IDstall, IDop, BEQ, EXop, LW, EXrn, IDrm); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, EXop=LW, EXrt=IDrt");
-   setupStall(IDstall, IDop, BEQ, EXop, LW, EXrt, IDrt); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=LW, EXrn=IDrn");
+   setupStall(IDstall, IDop, BEQ, EXop, LW, EXrn, IDrn); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, EXop=ADDI, EXrt=IDrs");
-   setupStall(IDstall, IDop, BEQ, EXop, ADDI, EXrt, IDrs); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=ADDI, EXrn=IDrm");
+   setupStall(IDstall, IDop, BEQ, EXop, ADDI, EXrn, IDrm); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, EXop=ADDI, EXrt=IDrt");
-   setupStall(IDstall, IDop, BEQ, EXop, ADDI, EXrt, IDrt); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=ADDI, EXrn=IDrn");
+   setupStall(IDstall, IDop, BEQ, EXop, ADDI, EXrn, IDrn); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, MEMop=LW, MEMrt=IDrs");
-   setupStall(IDstall, IDop, BEQ, MEMop, LW, MEMrt, IDrs); #10;
+   $display("[IDSTALL] IDop=BEQ, MEMop=LW, MEMrn=IDrm");
+   setupStall(IDstall, IDop, BEQ, MEMop, LW, MEMrn, IDrm); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, MEMop=LW, MEMrt=IDrt");
-   setupStall(IDstall, IDop, BEQ, MEMop, LW, MEMrt, IDrt); #10;
+   $display("[IDSTALL] IDop=BEQ, MEMop=LW, MEMrn=IDrn");
+   setupStall(IDstall, IDop, BEQ, MEMop, LW, MEMrn, IDrn); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BEQ, EXop=SLTI, EXrt=IDrs");
-   setupStall(IDstall, IDop, BEQ, EXop, SLTI, EXrt, IDrs); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=SLTI, EXrn=IDrm");
+   setupStall(IDstall, IDop, BEQ, EXop, SLTI, EXrn, IDrm); #10;
    verifyEqual(IDstall, 1);
    
-   $display("[IDSTALL] IDop=BEQ, EXop=SLTI, EXrt=IDrt");
-   setupStall(IDstall, IDop, BEQ, EXop, SLTI, EXrt, IDrt); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=SLTI, EXrn=IDrn");
+   setupStall(IDstall, IDop, BEQ, EXop, LUI, EXrn, IDrn); #10;
    verifyEqual(IDstall, 1);
 
-   $display("[IDSTALL] IDop=BNE, EXop=RTYPE, EXrd=IDrs");
-   setupStall(IDstall, IDop, BNE, EXop, RTYPE, EXrd, IDrs); #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, EXop=RTYPE, EXrd=IDrt");
-   setupStall(IDstall, IDop, BNE, EXop, RTYPE, EXrd, IDrt); #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, EXop=LW, EXrt=IDrs");
-   setupStall(IDstall, IDop, BNE, EXop, LW, EXrt, IDrs); #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, EXop=LW, EXrt=IDrt");
-   setupStall(IDstall, IDop, BNE, EXop, LW, EXrt, IDrt);  #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, EXop=ADDI, EXrt=IDrs");
-   setupStall(IDstall, IDop, BNE, EXop, ADDI, EXrt, IDrs); #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, EXop=ADDI, EXrt=IDrt");
-   setupStall(IDstall, IDop, BNE, EXop, ADDI, EXrt, IDrt); #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, MEMop=LW, MEMrt=IDrs");
-   setupStall(IDstall, IDop, BNE, MEMop, LW, MEMrt, IDrs); #10;
-   verifyEqual(IDstall, 1);
-
-   $display("[IDSTALL] IDop=BNE, MEMop=LW, MEMrt=IDrt");
-   setupStall(IDstall, IDop, BNE, MEMop, LW, MEMrt, IDrt); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=LUI, EXrn=IDrm");
+   setupStall(IDstall, IDop, BEQ, EXop, SLTI, EXrn, IDrm); #10;
    verifyEqual(IDstall, 1);
    
-   $display("[IDSTALL] IDop=BNE, EXop=SLTI, EXrt=IDrs");
-   setupStall(IDstall, IDop, BNE, EXop, SLTI, EXrt, IDrs); #10;
+   $display("[IDSTALL] IDop=BEQ, EXop=LUI, EXrn=IDrn");
+   setupStall(IDstall, IDop, BEQ, EXop, LUI, EXrn, IDrn); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=RTYPE, EXrp=IDrm");
+   setupStall(IDstall, IDop, BNE, EXop, RTYPE, EXrp, IDrm); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=RTYPE, EXrp=IDrn");
+   setupStall(IDstall, IDop, BNE, EXop, RTYPE, EXrp, IDrn); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=LW, EXrn=IDrm");
+   setupStall(IDstall, IDop, BNE, EXop, LW, EXrn, IDrm); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=LW, EXrn=IDrn");
+   setupStall(IDstall, IDop, BNE, EXop, LW, EXrn, IDrn);  #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=ADDI, EXrn=IDrm");
+   setupStall(IDstall, IDop, BNE, EXop, ADDI, EXrn, IDrm); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=ADDI, EXrn=IDrn");
+   setupStall(IDstall, IDop, BNE, EXop, ADDI, EXrn, IDrn); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, MEMop=LW, MEMrn=IDrm");
+   setupStall(IDstall, IDop, BNE, MEMop, LW, MEMrn, IDrm); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, MEMop=LW, MEMrn=IDrn");
+   setupStall(IDstall, IDop, BNE, MEMop, LW, MEMrn, IDrn); #10;
    verifyEqual(IDstall, 1);
    
-   $display("[IDSTALL] IDop=BNE, EXop=SLTI, EXrt=IDrt");
-   setupStall(IDstall, IDop, BNE, EXop, SLTI, EXrt, IDrt); #10;
+   $display("[IDSTALL] IDop=BNE, EXop=SLTI, EXrn=IDrm");
+   setupStall(IDstall, IDop, BNE, EXop, SLTI, EXrn, IDrm); #10;
+   verifyEqual(IDstall, 1);
+   
+   $display("[IDSTALL] IDop=BNE, EXop=SLTI, EXrn=IDrn");
+   setupStall(IDstall, IDop, BNE, EXop, SLTI, EXrn, IDrn); #10;
+   verifyEqual(IDstall, 1);
+
+   $display("[IDSTALL] IDop=BNE, EXop=LUI, EXrn=IDrm");
+   setupStall(IDstall, IDop, BNE, EXop, LUI, EXrn, IDrm); #10;
+   verifyEqual(IDstall, 1);
+   
+   $display("[IDSTALL] IDop=BNE, EXop=LUI, EXrn=IDrn");
+   setupStall(IDstall, IDop, BNE, EXop, LUI, EXrn, IDrn); #10;
    verifyEqual(IDstall, 1);
    ///////////////////////////////////////////////////////////////////////////////
 
    ///////////////////////////////////////////////////////////////////////////////
    // EXSTALL TESTS (Time: 20 each)
-   $display("[EXSTALL] MEMop=LW, EXop=RTYPE, MEMrt=EXrs");
-   setupStall(EXstall, MEMop, LW, EXop, RTYPE, MEMrt, EXrs); #10;
+   $display("[EXSTALL] MEMop=LW, EXop=RTYPE, MEMrn=EXrm");
+   setupStall(EXstall, MEMop, LW, EXop, RTYPE, MEMrn, EXrm); #10;
    verifyEqual(EXstall, 1);
 
-   $display("[EXSTALL] MEMop=LW, EXop=RTYPE, MEMrt=EXrt");
-   setupStall(EXstall, MEMop, LW, EXop, RTYPE, MEMrt, EXrt);  #10;
+   $display("[EXSTALL] MEMop=LW, EXop=RTYPE, MEMrn=EXrn");
+   setupStall(EXstall, MEMop, LW, EXop, RTYPE, MEMrn, EXrn);  #10;
    verifyEqual(EXstall, 1);
 
-   $display("[EXSTALL] MEMop=LW, EXop=LW, MEMrt=EXrs");
-   setupStall(EXstall, MEMop, LW, EXop, LW, MEMrt, EXrs); #10;
+   $display("[EXSTALL] MEMop=LW, EXop=LW, MEMrn=EXrm");
+   setupStall(EXstall, MEMop, LW, EXop, LW, MEMrn, EXrm); #10;
    verifyEqual(EXstall, 1);
 
-   $display("[EXSTALL] MEMop=LW, EXop=SW, MEMrt=EXrs");
-   setupStall(EXstall, MEMop, LW, EXop, SW, MEMrt, EXrs); #10;
+   $display("[EXSTALL] MEMop=LW, EXop=SW, MEMrn=EXrm");
+   setupStall(EXstall, MEMop, LW, EXop, SW, MEMrn, EXrm); #10;
    verifyEqual(EXstall, 1);
 
-   $display("[EXSTALL] MEMop=LW, EXop=ADDI, MEMrt=EXrs");
-   setupStall(EXstall, MEMop, LW, EXop, ADDI, MEMrt, EXrs); #10;
+   $display("[EXSTALL] MEMop=LW, EXop=ADDI, MEMrn=EXrm");
+   setupStall(EXstall, MEMop, LW, EXop, ADDI, MEMrn, EXrm); #10;
    verifyEqual(EXstall, 1);
 
-   $display("[EXSTALL] MEMop=LW, EXop=SLTI, MEMrt=EXrs");
-   setupStall(EXstall, MEMop, LW, EXop, SLTI, MEMrt, EXrs); #10;
+   $display("[EXSTALL] MEMop=LW, EXop=SLTI, MEMrn=EXrm");
+   setupStall(EXstall, MEMop, LW, EXop, SLTI, MEMrn, EXrm); #10;
    verifyEqual(EXstall, 1);
    ///////////////////////////////////////////////////////////////////////////////
    $display("All tests passed.");
